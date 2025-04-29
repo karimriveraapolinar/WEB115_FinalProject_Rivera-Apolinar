edfa52ed3029f8a6cd9e36f166fa26515f29769e
karimriveraapolinar <kariveraapolinar@my.waketech.edu>
2025-04-29 14:01:28 -0400

    final push

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const taskName = document.getElementById("taskName");
 const submitButton = document.getElementById("submitButton");
 const result = document.getElementById("result");
-//const priority = document.getElementById("priority");//
+const priority = document.getElementById("importance");//
 let task;
+let importance
 submitButton.onclick = function () {
     task = taskName.value;
     task = String(task)
+    importance = priority.value;
+    importance = String(importance)
     result.textContent = ` ${task}`
-    result.textContent += `Hello`
+    result.textContent += `${importance}`
 }
+     
