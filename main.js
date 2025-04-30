760bc59816ea0be73af21317cbb865ff4af35865
karimriveraapolinar <kariveraapolinar@my.waketech.edu>
2025-04-30 14:01:03 -0400

    today

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
 const taskName = document.getElementById("taskName");
 const submitButton = document.getElementById("submitButton");
-const result = document.getElementById("result");
-const priority = document.getElementById("importance");//
+const taskmanager = document.getElementById("taskmanager");
+const priority = document.getElementById("Priority");
+const Importance = document.getElementById("Importance")
 let task;
-let importance
+let importance;
+let Important;
 submitButton.onclick = function () {
-    task = taskName.value;
-    task = String(task)
-    importance = priority.value;
-    importance = String(importance)
-    result.textContent = ` ${task}`
-    result.textContent += `${importance}`
-}
-     
+  task = taskName.value;
+  task = String(task);
+  importance = priority.value;
+  importance = String(importance);
+  Important = Importance.value;
+  Important = Boolean(Important)
+  if (Important=true) {
+    taskmanager
+  }
+taskmanager.textContent = `${task}                        ${importance}                         ${Important}  `;
+};
+
+let output = {
+  id: 1,
+  name: "Finish JavaScript project",
+  priority: "High",
+  isImportant: true,
+  isCompleted: false,
+  date: today,
+};
+output.push();
+
+if (document.getElementById("Importance").checked){
+
+    
+    }
